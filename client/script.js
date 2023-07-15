091f6f784b2127e1e052ce341a8857edb1fd2d48
Sauravastic <85219406+Sauravastic@users.noreply.github.com>
2023-07-15 20:45:02 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://sauravasticAI.onrender.com', {
+    const response = await fetch('https://sauravasticai.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
